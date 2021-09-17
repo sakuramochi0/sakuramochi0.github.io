002c3e07880cf822cb95d61faf4b357e4498270f
sakuramochi <sakuramochi.mochi.0@gmail.com>
2021-09-17 13:32:09 +0900

    Create a minimal home page

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+	name: "Home",
+};
+</script>
+
+<template>
+	<img alt="Rainbow emoji" src="src/assets/rainbow.svg" width="100" />
+	<h1>さくらもち<br />(sakuramochi)</h1>
+	<p>GitHub: <a href="https://github.com/sakuramochi0">@sakuramochi0 </a></p>
+	<p>
+		Twitter (archive):
+		<a
+			href="https://storage.googleapis.com/twitter-archive-skrmch_prism/index.html#/tweets/tweets"
+			>@skrmch_prism</a
+		>
+	</p>
+</template>
+
+<style>
+#app {
+	font-family: Roboto, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin: 60px auto;
+}
+</style>
