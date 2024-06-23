87a9504f3e70a95a9d02cb855768d2de424b0277
sakuramochi0 <sakuramochi.mochi.0@gmail.com>
2024-06-24 00:00:34 +0900

    chore: modernize prettier config

---
+++ prettier.config.js
@@ -0,0 +1,29 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  semi: false,
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        printWidth: 70,
+        useTabs: false,
+        trailingComma: 'none',
+        arrowParens: 'avoid',
+        proseWrap: 'never',
+      },
+    },
+    {
+      files: '*.{json,babelrc,eslintrc,remarkrc,prettierrc}',
+      options: {
+        useTabs: false,
+      },
+    },
+  ],
+}
+
+export default config;
